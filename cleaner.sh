7fb86b293b6a4fa7a3b0ea448bd8e0a2aedb3e8c
Óscar Nájera <najera.oscar@gmail.com>
2013-07-31 20:55:12 -0500

    Custom cleaner, cleans files acoording to subject

---
+++ cleaner.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+# Various scripts to remove backup and unnecessary files
+usage(){
+    echo "Usage: $0 mode"
+    echo "mode options:"
+    echo "    latex      cleans latex temporary files"
+}
+
+#Cleaning modules
+latex(){
+EXTENSIONS="*.tex~ *.tex.backup *.dvi *.log *.pdf"
+for dir in $(ls -d */ ./)
+do
+    cd $dir
+    rm -vf $EXTENSIONS
+done
+}
+
+#Run script
+[[ $# -eq 0 ]] && usage
+$1 
--- latexclean.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-EXTENSIONS="*.tex~ *.tex.backup *.dvi *.log *.pdf"
-for dir in $(ls -d */)
-do
-    cd $dir
-    rm -vf $EXTENSIONS
-done
-rm -vf $EXTENSIONS
