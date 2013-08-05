464403ee129b13a7c28a9e10a6cb5d766f940154
Óscar Nájera <najera.oscar@gmail.com>
2013-08-05 18:14:46 -0500

    Simple backup

--- backup.sh
+++ backup.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
-rsync -av ~/* 
+rsync -av --del ~/ /run/media/oscar/BackUP/oscar/ --exclude={Downloads,Videos,Archiver,Music,.*}
 
+rsync -av --remove-source-files ~/Archiver/ /run/media/oscar/BackUP/oscar/Archiver/
 
