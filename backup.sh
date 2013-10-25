baff384a59ea85277b19084c99af96d2664ff8fb
Óscar Nájera <najera.oscar@gmail.com>
2013-10-25 15:43:11 +0200

    Script to act as hotspot for wifi

--- backup.sh
+++ backup.sh
@@ -3,4 +3,4 @@
 rsync -av --del ~/ /run/media/oscar/BackUP/oscar/ --exclude={Downloads,Videos,Archiver,Music,.*}
 
 rsync -av --remove-source-files ~/Archiver/ /run/media/oscar/BackUP/oscar/Archiver/
-
+rm -r ~/Archiver/*
