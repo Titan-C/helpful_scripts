f4abd4e6ec30570b0fb9a6c25a9641bb02a694d1
Oscar Nájera <najera.oscar@gmail.com>
2015-02-09 19:28:59 +0100

    fftw installer

--- localinstall.sh
+++ localinstall.sh
@@ -1,21 +1,31 @@
 #! /bin/bash
 
 LIB=~/libs/
-
-mkdir $LIB
+mkdir ${LIB}
 
 
 ## cmake
 cmakev=cmake-3.1.2
-wget http://www.cmake.org/files/v3.1/$cmake.tar.gz  -O cmake.tar.gz
+wget http://www.cmake.org/files/v3.1/${cmake}.tar.gz  -O cmake.tar.gz
 tar -xf cmake.tar.gz
-cd $cmakev
-./bootstrap --parallel=4 --prefix=$LIB
+cd ${cmakev}
+./bootstrap --parallel=4 --prefix=${LIB}
 make
 make install
+cd
 
 ## boost
 ## fftw
+fftwv=fftw-3.3.4
+wget http://www.fftw.org/${fftwv}.tar.gz
+tar -xf ${fftwv}.tar.gz
+cd ${fftwv}
+./configure --prefix=${LIB} --enable-openmp --enable-mpi --enable-sse --enable-sse2 --enable-avx CFLAGS="-O2"
+make -j8
+make install
+cd
+
+
 ## Anaconda
 wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
 chmod +x miniconda.sh
