177d9e5db63c429c5584a51bfe54a9dabaf7d712
Óscar Nájera <najera.oscar@gmail.com>
2015-07-05 20:26:14 +0200

    joblib conda install

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-rsync -av --del ~/Documents ~/Dropbox ~/Pictures /run/media/oscar/BackUP/oscar/
-rsync -av ~/Music ~/Videos /run/media/oscar/BackUP/oscar/
+rsync -av --del ~/Documents ~/Dropbox ~/Pictures /media/oscar/oscar/
+rsync -av ~/Music ~/Videos /media/oscar/oscar/
--- localinstall.sh
+++ localinstall.sh
@@ -157,7 +157,8 @@ export PATH=~/miniconda/bin:$PATH
 conda update --yes conda
 conda create --yes -n dev pip scipy numpy matplotlib hdf5 h5py ipython \
     jinja2 numba pep8 pillow pyflakes pytest cython numba \
-    sphinx spyder coverage nose rope tornado jsonschema numpydoc mistune
+    sphinx spyder coverage nose rope tornado jsonschema numpydoc mistune \
+    joblib
 source activate dev
 pip install mako
 pip install mpi4py
