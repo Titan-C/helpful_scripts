bcadcaf906e04ca1b40fe5db506413148b9138e8
Óscar Nájera <najera.oscar@gmail.com>
2015-10-18 23:45:59 +0200

    Anaconda and Emacs start on login

--- autostart.sh
+++ autostart.sh
@@ -1,4 +1,21 @@
 #! /bin/bash
 
+SCRIPTS=$HOME/dev/helpful_scripts/xkb/
 # Sets my custom keyboard layout
-setxkbmap -I$HOME/dev/helpful_scripts/xkb/ dvorakprog -option caps:escape -geometry 'microsoft(natural)' -print | xkbcomp -I$HOME/dev/helpful_scripts/xkb/ - $DISPLAY
+setxkbmap -I$SCRIPTS dvorakprog -option caps:escape -geometry 'microsoft(natural)' -print | xkbcomp -I$SCRIPTS - $DISPLAY
+
+# Set anaconda
+anacondainit() {
+    export PATH=$HOME/miniconda/bin:$SCRIPTS:$PATH
+    export QT_PLUGIN_PATH=""
+    # Local libraries
+    export PATH=$HOME/libs/bin:$PATH
+    export LD_LIBRARY_PATH=$HOME/libs/lib:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=$HOME/libs/lib64:$LD_LIBRARY_PATH
+    export PYTHONPATH=$HOME/libs/lib:$PYTHONPATH
+    export OPENBLAS_NUM_THREADS=1
+}
+
+anacondainit
+source activate dev
+emacs &
