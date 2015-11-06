c8c7de25a8e8f6ad2b690bed505194b057b8e53d
Óscar Nájera <najera.oscar@gmail.com>
2015-11-06 13:58:40 +0100

    Emacs startup to dev venv

--- autostart.sh
+++ autostart.sh
@@ -2,20 +2,13 @@
 
 SCRIPTS=$HOME/dev/helpful_scripts
 # Set anaconda
-anacondainit() {
-    export PATH=$HOME/miniconda/bin:$SCRIPTS:$PATH
-    export QT_PLUGIN_PATH=""
-    # Local libraries
-    export PATH=$HOME/libs/bin:$PATH
-    export LD_LIBRARY_PATH=$HOME/libs/lib:$LD_LIBRARY_PATH
-    export LD_LIBRARY_PATH=$HOME/libs/lib64:$LD_LIBRARY_PATH
-    export PYTHONPATH=$HOME/libs/lib:$PYTHONPATH
-    export OPENBLAS_NUM_THREADS=1
-}
-
+export PATH=$HOME/miniconda3/bin:$SCRIPTS:$PATH
+export QT_PLUGIN_PATH=""
 
 $SCRIPTS/keymaps.sh
-anacondainit
 $SCRIPTS/keylogger.sh
+
 source activate dev
+export LD_LIBRARY_PATH=$CONDA_ENV_PATH/lib
+export OPENBLAS_NUM_THREADS=1
 emacs &
