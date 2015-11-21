c51c7b86a2928573be13025fce697f19c2e3d385
Óscar Nájera <najera.oscar@gmail.com>
2015-11-21 23:13:23 +0100

    Debuging org configuration

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -15,8 +15,6 @@
     (setq use-package-verbose t
           use-package-always-ensure t))
 
-;;; Load the config
-(org-babel-load-file (concat user-emacs-directory "config.org"))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -26,3 +24,6 @@
    (quote
     ("c86f868347919095aa44d2a6129dd714cbcf8feaa88ba954f636295b14ceff8f" "9ff70d8009ce8da6fa204e803022f8160c700503b6029a8d8880a7a78c5ff2e5" default)))
  '(initial-frame-alist (quote ((fullscreen . maximized)))))
+
+;;; Load the config
+(org-babel-load-file (concat user-emacs-directory "config.org"))
