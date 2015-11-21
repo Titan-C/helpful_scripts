a947aa94bfba62e48f06bef89fdb18367a227b75
Óscar Nájera <najera.oscar@gmail.com>
2015-11-21 23:03:02 +0100

    Sorting the configuration emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -25,13 +25,4 @@
  '(custom-safe-themes
    (quote
     ("c86f868347919095aa44d2a6129dd714cbcf8feaa88ba954f636295b14ceff8f" "9ff70d8009ce8da6fa204e803022f8160c700503b6029a8d8880a7a78c5ff2e5" default)))
- '(initial-frame-alist (quote ((fullscreen . maximized))))
- '(send-mail-function (quote smtpmail-send-it))
- '(smtpmail-smtp-server "smtp.googlemail.com")
- '(smtpmail-smtp-service 465 t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+ '(initial-frame-alist (quote ((fullscreen . maximized)))))
