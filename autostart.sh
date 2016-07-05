ede20ffa3b71e5f1f034694315fa5922068df60d
Óscar Nájera <najera.oscar@gmail.com>
2016-07-05 19:52:20 +0200

    Don't load anaconda and Emacs on system start
    I do tend to restart Emacs more often than my average time of an open
    session. Thus it makes little sense to have this autostart with anaconda
    and emacs I can do it manually.
    
    Second a better way to use the anaconda environment is to use pyvenv,
    and not this launch script

--- autostart.sh
+++ autostart.sh
@@ -1,12 +1,5 @@
 #! /bin/bash
 
 SCRIPTS=$HOME/dev/helpful_scripts
-# Set anaconda
-export PATH=$HOME/miniconda3/bin:$SCRIPTS:$PATH
-export QT_PLUGIN_PATH=""
-
 $SCRIPTS/keymaps.sh
 $SCRIPTS/keylogger.sh
-
-source activate hpc3
-emacs &
