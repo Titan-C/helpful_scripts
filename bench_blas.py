d16714490d1c37a080d1e6feb1a22f28f9741667
Óscar Nájera <najera.oscar@gmail.com>
2016-08-01 19:06:42 +0200

    Blas benchmark dynamically setting thread numbers

--- bench_blas.py
+++ bench_blas.py
@@ -7,12 +7,43 @@ Quick benckmark that OpenBlas is used for numpy
 # Author: Óscar Nájera
 
 from __future__ import division, absolute_import, print_function
-from IPython import get_ipython
+import ctypes
 import numpy as np
-
+from IPython import get_ipython
 np.__config__.show()
 
-for N in [1000, 2000, 4000]:
-    a = np.random.rand(N, N)
-    print("Matrix multiplication N=" + str(N))
-    get_ipython().run_cell('time c = np.dot(a, a)')
+
+def num_threads(cores):
+    try:
+        cdll = ctypes.CDLL('libmkl_rt.so')
+        cdll.mkl_set_num_threads(ctypes.byref(ctypes.c_int(cores)))
+        print(cdll.mkl_get_max_threads())
+        return
+    except OSError:
+        pass
+
+    try:
+        cdll = ctypes.CDLL('libopenblas.so')
+        cdll.openblas_set_num_threads(int(cores))
+        print(cdll.openblas_get_num_threads())
+        return
+    except OSError:
+        pass
+
+    raise OSError('Unknow blas library')
+
+
+def run_bench(threads=None):
+    if threads is not None:
+        num_threads(threads)
+    for N in [1000, 2000, 4000]:
+        a = np.random.rand(N, N)
+        print("Matrix multiplication N=" + str(N))
+        get_ipython().magic('time c = np.dot(a, a)')
+
+print('\n On system default thread numbers')
+run_bench()
+print('\n\n Forced to single thread')
+run_bench(1)
+print('\n\n Forced to two threads')
+run_bench(2)
