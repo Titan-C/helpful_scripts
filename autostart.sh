491bad5564d779c29fb8cc872e40d4eff9bee436
Óscar Nájera <najera.oscar@gmail.com>
2016-08-02 00:46:02 +0200

    Save Xdbus to access is from the cronjob

--- autostart.sh
+++ autostart.sh
@@ -3,3 +3,9 @@
 SCRIPTS=$HOME/dev/helpful_scripts
 $SCRIPTS/keymaps.sh
 $SCRIPTS/keylogger.sh
+
+# save dbus session info so I can source it from mbsyncrc cronjob
+touch /tmp/.Xdbus
+chmod 600 /tmp/.Xdbus
+env | grep DBUS_SESSION_BUS_ADDRESS > /tmp/.Xdbus
+echo 'export DBUS_SESSION_BUS_ADDRESS' >> /tmp/.Xdbus
