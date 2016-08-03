245f65060b229e3ea4610f43e44691c536c9e946
Óscar Nájera <najera.oscar@gmail.com>
2016-08-03 13:48:51 +0200

    Mail download setup

---
+++ tagmail.sh
@@ -0,0 +1,7 @@
+
+notmuch search --output=files tag:deleted | xargs  -I {} rm -v "{}"
+
+notmuch new
+notmuch tag +clusjobs -unread -inbox from:root@local.u-psud.fr subject:Job tag:inbox
+notmuch tag +coding -inbox from:github.com or from:bitbucket.org or from:gitlab.com tag:inbox
+notmuch tag +ci -inbox from:travis-ci.com or from:travis-ci.org tag:inbox
