0c81320ee37056ee61eb1efcee674448f4311d31
Óscar Nájera <najera.oscar@gmail.com>
2016-08-10 18:35:50 +0200

    Import the zenburn theme

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -38,7 +38,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init("/usr/share/awesome/themes/zenburn/theme.lua")
+beautiful.init(os.getenv("HOME") .. "/.config/awesome/zenburn/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "urxvt"
