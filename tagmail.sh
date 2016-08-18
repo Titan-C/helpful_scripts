0e934fe87faee18c0e56aaf06ab1ed09b6e18d3b
Óscar Nájera <najera.oscar@gmail.com>
2016-08-19 00:54:17 +0200

    Delete mail locally then sync to server

--- tagmail.sh
+++ tagmail.sh
@@ -2,12 +2,13 @@ if [[ -z $DBUS_SESSION_BUS_ADDRESS ]]; then
     source /tmp/.Xdbus
 fi
 
+# delete mail
+notmuch search --output=files tag:deleted | xargs  -I {} rm -v "{}"
+
 # Mail download
 mbsync -aV
 
-# Notmuch
-notmuch search --output=files tag:deleted | xargs  -I {} rm -v "{}"
-
+# Notmuch tagging
 notmuch new
 notmuch tag +clusjobs -unread -inbox from:root@local.u-psud.fr subject:Job tag:inbox
 notmuch tag +coding -inbox from:github.com or from:bitbucket.org or from:gitlab.com tag:inbox
