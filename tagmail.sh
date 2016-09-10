9292e18c56405ebbf36444ed86b69d628cd21354
Óscar Nájera <najera.oscar@gmail.com>
2016-09-10 11:55:02 +0200

    Tagmail tome

--- tagmail.sh
+++ tagmail.sh
@@ -10,6 +10,8 @@ mbsync -aV
 
 # Notmuch tagging
 notmuch new
-notmuch tag +clusjobs -inbox from:root@local.u-psud.fr or from:root@orlando.u-psud.fr subject:Job tag:inbox
-notmuch tag +coding -inbox from:github.com or from:bitbucket.org or from:gitlab.com tag:inbox
-notmuch tag +ci -inbox from:travis-ci.com or from:travis-ci.org tag:inbox
+notmuch tag +clusjobs -inbox -tome from:root@local.u-psud.fr or from:root@orlando.u-psud.fr subject:Job tag:inbox
+notmuch tag +coding -inbox -tome from:github.com or from:bitbucket.org or from:gitlab.com tag:inbox
+notmuch tag +ci -inbox -tome from:travis-ci.com or from:travis-ci.org tag:inbox
+notmuch tag +LPS -inbox -tome subject:tous.lps tag:inbox
+notmuch tag +LPSPHD -inbox -tome subject:tousthesards.lps tag:inbox
