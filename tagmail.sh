44f2dbe3a3b67fdf326ad2ea3c914441a1a1e5ed
Óscar Nájera <najera.oscar@gmail.com>
2016-12-21 17:42:01 +0100

    Add circle ci filter

--- tagmail.sh
+++ tagmail.sh
@@ -11,7 +11,7 @@ mbsync -aV
 # Notmuch tagging
 notmuch new
 notmuch tag +clusjobs -inbox from:root@local.u-psud.fr or from:root@orlando.u-psud.fr subject:Job tag:inbox
-notmuch tag +coding -inbox from:github.com or from:bitbucket.org or from:gitlab.com tag:inbox
-notmuch tag +ci -inbox from:travis-ci.com or from:travis-ci.org or from:appveyor.com tag:inbox
+notmuch tag +coding -inbox from:github.com or from:bitbucket.org or from:mg.gitlab.com tag:inbox
+notmuch tag +ci -inbox from:travis-ci.com or from:travis-ci.org or from:appveyor.com or from:circleci.com tag:inbox
 notmuch tag +LPS -inbox subject:tous.lps tag:inbox
 notmuch tag +LPSPHD -inbox subject:tousthesards.lps tag:inbox
