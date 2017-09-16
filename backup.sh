2da3475be64e661551358ac83116820cf0864f9d
Óscar Nájera <hello@oscarnajera.com>
2017-09-16 19:29:52 +0200

    Improve backup with borgbackup

--- backup.sh
+++ backup.sh
@@ -1,3 +1,20 @@
 #! /bin/bash
 
-rsync -av --del ~/Documents ~/Dropbox ~/MEGA /media/oscar/oscar/
+rsync -av --del ~/Documents ~/Dropbox ~/MEGA /scratch/oscar/
+
+# external program to supply the passphrase:
+export BORG_PASSCOMMAND='pass show borgbackup'
+
+# Create Backups
+echo "Creating Backups ..."
+REPOSITORY=/scratch/Backups/code_dev
+borg create --verbose --stats --compression=lz4 \
+     --exclude '*.pyc' \
+    $REPOSITORY::'{hostname}-{user}-{utcnow:%Y-%m-%dT%H:%M:%S}' \
+    ~/dev ~/.mail/ ~/Documents/
+
+echo "Creating image Backups ..."
+borg create --verbose --stats --compression=lz4 \
+     --exclude '*.pyc' \
+    $REPOSITORY::'{hostname}-pics-{utcnow:%Y-%m-%dT%H:%M:%S}' \
+    ~/MEGA/Pictures/
