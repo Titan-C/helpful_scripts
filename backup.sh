965e038d5a3efc0728480d269767304573c0cc2c
Óscar Nájera <hello@oscarnajera.com>
2017-10-29 20:04:42 +0100

    unify backup

--- backup.sh
+++ backup.sh
@@ -11,10 +11,4 @@ REPOSITORY=/scratch/Backups/code_dev
 borg create --verbose --stats --compression=lz4 \
      --exclude '*.pyc' \
     $REPOSITORY::'{hostname}-{user}-{utcnow:%Y-%m-%dT%H:%M:%S}' \
-    ~/dev ~/.mail/ ~/Documents/
-
-echo "Creating image Backups ..."
-borg create --verbose --stats --compression=lz4 \
-     --exclude '*.pyc' \
-    $REPOSITORY::'{hostname}-pics-{utcnow:%Y-%m-%dT%H:%M:%S}' \
-    ~/MEGA/Pictures/
+    ~/dev ~/.mail/ ~/Documents/ ~/Dropbox/ ~/ownCloud/ ~/MEGA/
