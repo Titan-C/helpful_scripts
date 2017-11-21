0b5cbd45fe8bb5bd60ffb67ba4046f713b45027c
Óscar Nájera <najera.oscar@gmail.com>
2017-11-21 15:59:08 +0100

    cleanup files

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -21,18 +21,6 @@
     (setq use-package-verbose t
           use-package-always-ensure t))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("c86f868347919095aa44d2a6129dd714cbcf8feaa88ba954f636295b14ceff8f" "9ff70d8009ce8da6fa204e803022f8160c700503b6029a8d8880a7a78c5ff2e5" default)))
- '(package-selected-packages
-   (quote
-    (org-pdfview helm-notmuch notmuch helm-org-rifle racer org-ref helm-bibtex key-chord hydra org-edit-latex evil-surround yaml-mode which-key use-package twittering-mode swiper sphinx-doc spaceline smartparens scss-mode relative-line-numbers rainbow-delimiters pythonic py-autopep8 ox-reveal org-plus-contrib org-gcal org-bullets multi-term material-theme markdown-mode magit-gh-pulls lua-mode langtool htmlize helm-pydoc helm-projectile helm-ag flycheck-rust flycheck-clojure exec-path-from-shell evil-leader eshell-git-prompt emms elpy elfeed-org ein dockerfile-mode cython-mode company-math command-log-mode cmake-mode cargo bbdb auto-complete auctex ace-window))))
-
 ;;; Load the config
 (org-babel-load-file (concat user-emacs-directory "config.org"))
 
@@ -61,9 +49,3 @@
 ;;(erc-tls :server "irc.mozilla.org" :port "6697" :nick "titan-c")
 
 ;;(erc-tls :server "irc.gitter.im" :port 6697 :password (cadr (auth-source-user-and-password "irc.gitter.im")) :nick (car (auth-source-user-and-password "irc.gitter.im")))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
