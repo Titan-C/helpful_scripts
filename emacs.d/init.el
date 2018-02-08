c8056724d031336b00332681f6585d7479428249
Óscar Nájera <hello@oscarnajera.com>
2018-02-08 10:34:04 -0500

    use org elpa

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -9,6 +9,7 @@
 (require 'package)
 (setq package-archives '(;;("melpast" . "https://stable.melpa.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
                          ("gnu" . "https://elpa.gnu.org/packages/")))
 (package-initialize)
 
