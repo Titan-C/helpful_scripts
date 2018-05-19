542dd5417c304f76c2648aa4857268bca658e635
Óscar Nájera <hello@oscarnajera.com>
2018-05-19 17:11:54 +0200

    Clean init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2,7 +2,7 @@
 (setq gc-cons-threshold 100000000)
 (scroll-bar-mode 0)
 (tool-bar-mode 0)
-(add-to-list 'default-frame-alist '(alpha . (95 . 70)))
+;;(add-to-list 'default-frame-alist '(alpha . (95 . 70)))
 (setq tls-checktrust t)
 (setq gnutls-verify-error t)
 
@@ -24,26 +24,6 @@
 
 ;;; Load the config
 (org-babel-load-file (concat user-emacs-directory "config.org"))
-
-(defun auth-source-user-and-password (host &optional user)
-  (let* ((auth-info (car
-                     (if user
-                         (auth-source-search
-                          :host host
-                          :user user
-                          :max 1
-                          :require '(:user :secret)
-                          :create nil)
-                       (auth-source-search
-                        :host host
-                        :max 1
-                        :require '(:user :secret)
-                        :create nil))))
-         (user (plist-get auth-info :user))
-         (password (plist-get auth-info :secret)))
-    (when (functionp password)
-      (setq password (funcall password)))
-    (list user password auth-info)))
 
 ;;(erc-tls :server "irc.oftc.net" :port "6697" :nick "titan-c")
 ;;(erc-tls :server "chat.freenode.net" :port "6697" :nick (car (auth-source-user-and-password "chat.freenode.net")) :password  (cadr (auth-source-user-and-password "chat.freenode.net")))
