233abc5419e2a6c70d080e1f13f5d6c9ecfab442
Óscar Nájera <hello@oscarnajera.com>
2018-09-09 20:24:52 +0200

    Move package management to org file

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2,25 +2,11 @@
 (setq gc-cons-threshold 100000000)
 (scroll-bar-mode 0)
 (tool-bar-mode 0)
+(menu-bar-mode 0)
 ;;(add-to-list 'default-frame-alist '(alpha . (95 . 70)))
 (setq tls-checktrust t)
 (setq gnutls-verify-error t)
 
-(require 'package)
-(setq package-archives '(("melpast" . "https://stable.melpa.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
-                         ("gnu" . "https://elpa.gnu.org/packages/")))
-(package-initialize)
-
-(unless (package-installed-p 'use-package)
-   (package-refresh-contents)
-   (package-install 'use-package))
-
-(eval-when-compile
-    (require 'use-package)
-    (setq use-package-verbose t
-          use-package-always-ensure t))
 
 ;;; Load the config
 (org-babel-load-file (concat user-emacs-directory "config.org"))
