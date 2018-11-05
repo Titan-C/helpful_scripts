861c682c05f921d889935edf19fc8be09ff17bd4
Óscar Nájera <hello@oscarnajera.com>
2018-11-05 14:08:55 +0100

    Some extra email filters

--- tagmail.sh
+++ tagmail.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 if [[ -z $DBUS_SESSION_BUS_ADDRESS ]]; then
     source /tmp/.Xdbus
 fi
@@ -13,15 +15,23 @@ notmuch new
 
 notmuch tag +ci -new -- from:travis-ci.com or from:travis-ci.org or from:appveyor.com or from:circleci.com
 notmuch tag +ci -new -- from:mg.gitlab.com and subject:Pipeline
-notmuch tag +cmk-commits -new -- to:cmk-internal-commits@mathias-kettner.de
+notmuch tag +CMK-JIRA -new -- subject:\[CMK-JIRA\]
+notmuch tag +alle-mk -new -- to:alle@mathias-kettner.de
+notmuch tag +cmk-commits -new -- to:cmk-internal-commits@mathias-kettner.de and subject:\[cmk\]
+notmuch tag +mkde-commits -new -- to:cmk-internal-commits@mathias-kettner.de and subject:\[mkde\]
+notmuch tag +cmc-commits -new -- to:cmk-internal-commits@mathias-kettner.de and subject:\[cmc\]
+notmuch tag +cma-commits -new -- to:cma-commits@mathias-kettner.de and subject:\[cma\]
 notmuch tag +immonews -new -- from:immobilienscout24.de
 notmuch tag +immonews/communications -new -- from:nachrichten.immobilienscout24.de or subject:Kontaktaufnahme and tag:immonews
 notmuch tag +immonews -new -- from:immobilienscout24.de and subject:Kontact
 notmuch tag +socialnews -new -- from:facebookmail.com or from:mail.instagram.com
 notmuch tag +linkedin +socialnews -new -- from:linkedin.com
-notmuch tag +CMK-JIRA -new -- subject:\[CMK-JIRA\]
-notmuch tag +alle-mk -new -- to:alle@mathias-kettner.de
 notmuch tag +promotions -- from:newsletter
+
+notmuch tag +sms -inbox -- folder:hi_pers/SMS
+notmuch tag +calls -inbox -- folder:hi_pers/Calls
 
 # afew tagging
 $HOME/.local/bin/afew --tag --new -v
+
+notmuch tag -inbox -- tag:lists
