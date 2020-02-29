199be4283ccde52ebc693c954a84b932e99c5b1d
Óscar Nájera <hello@oscarnajera.com>
2020-02-29 12:44:35 +0100

    mailtags

--- tagmail.sh
+++ tagmail.sh
@@ -15,15 +15,14 @@ notmuch new
 
 notmuch tag +ci -new -- from:travis-ci.com or from:travis-ci.org or from:appveyor.com or from:circleci.com
 notmuch tag +ci -new -- from:mg.gitlab.com and subject:Pipeline
-notmuch tag +CMK-JIRA -new -- subject:\[CMK-JIRA\] or subject:\[tribe29 Jira\]
-notmuch tag +alle-mk -new -- to:alle@mathias-kettner.de
+notmuch tag +CMK-JIRA -inbox -new -- from:infra@mathias-kettner.de or from:jira@tribe29.com
+notmuch tag +alle-mk -new -- to:alle@mathias-kettner.de to:technik@tribe29.com
 notmuch tag +cmk-commits -new -- to:cmk-internal-commits@mathias-kettner.de and subject:\[cmk\]
 notmuch tag +mkde-commits -new -- to:cmk-internal-commits@mathias-kettner.de and subject:\[mkde\]
 notmuch tag +cmc-commits -new -- to:cmk-internal-commits@mathias-kettner.de and subject:\[cmc\]
 notmuch tag +cma-commits -new -- to:cma-commits@mathias-kettner.de and subject:\[cma\]
 notmuch tag +immonews -new -- from:immobilienscout24.de
 notmuch tag +immonews/communications -new -- from:nachrichten.immobilienscout24.de or subject:Kontaktaufnahme and tag:immonews
-notmuch tag +immonews -new -- from:immobilienscout24.de and subject:Kontact
 notmuch tag +socialnews -new -- from:facebookmail.com or from:mail.instagram.com
 notmuch tag +linkedin +socialnews -new -- from:linkedin.com
 notmuch tag +promotions -- from:newsletter
