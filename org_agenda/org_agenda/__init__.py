21650b2bc5dd6fee04242d635b991036e2373177
Óscar Nájera <hello@oscarnajera.com>
2020-03-15 10:56:07 +0100

    Force download flag

--- org_agenda/org_agenda/__init__.py
+++ org_agenda/org_agenda/__init__.py
@@ -9,6 +9,7 @@ Goal of the script
 # Author: Óscar Nájera
 # License:GPL-3
 
+import argparse
 import configparser
 import hashlib
 import json
@@ -31,21 +32,24 @@ def passwordstore(address: str) -> str:
         return process.stdout.split()[0]
 
 
-def get_icalendar(section):
+def get_icalendar(calendar, section, force):
     url = section["url"]
     hash_url = hashlib.sha1(url.encode("UTF-8")).hexdigest()[:10]
     url_file = f"/tmp/agenda-{hash_url}"
-    if os.path.exists(url_file):
+    if not force and os.path.exists(url_file):
         with open(url_file) as txt:
             return Calendar.from_ical(txt.read())
 
     user = section["user"]
     password = passwordstore(section["passwordstore"])
 
+    logger = logging.getLogger("Org_calendar")
+    logger.info("Downloading Calendar: %s", calendar)
+
     ical_reply = requests.get(url, auth=(user, password))
 
     if ical_reply.status_code == 200:
-        with open(url_file, 'w') as txt:
+        with open(url_file, "w") as txt:
             txt.write(ical_reply.text)
         return Calendar.from_ical(ical_reply.text)
 
@@ -55,27 +59,39 @@ def get_config():
     config = configparser.ConfigParser()
 
     logger = logging.getLogger("Org_calendar")
-    config_file = os.path.expanduser('~/.calendars.conf')
+    config_file = os.path.expanduser("~/.calendars.conf")
     logger.info("Reading config from: %s", config_file)
     config.read([config_file])
 
     return json.loads(json.dumps(config._sections))
 
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="Translate CalDav Agenda to orgfile")
+    parser.add_argument("-f",
+                        "--force",
+                        help="Force Download of Caldav files",
+                        action="store_true")
+    parser.add_argument("-v", "--verbose", action="count", default=0)
+
+    return parser.parse_args()
+
+
 def main():
+    args = parse_arguments()
 
     logger = logging.getLogger("Org_calendar")
     logger.addHandler(logging.StreamHandler())
-    logger.setLevel(logging.INFO)
+    log_level = logging.INFO if args.verbose > 0 else logging.WARNING
+    logger.setLevel(log_level)
 
     config = get_config()
     defaults = config.pop("defaults")
 
     events = []
     for calendar in config:
-        logger.info("Downloading Calendar: %s", calendar)
-        ical = get_icalendar(config[calendar])
-        logger.info("Finished Downloading Calendar: %s", calendar)
+        ical = get_icalendar(calendar, config[calendar], args.force)
         events += [
             ical2org.orgEntry(entry) for entry in ical.walk()
             if entry.name == "VEVENT"
