5f3cd2a72bbeb945a7649a022d7d563a436a3630
Óscar Nájera <hi@oscarnajera.com>
2020-08-09 13:04:37 +0200

    move keymaps to dotfiles

--- keymaps.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-SCRIPTS=$HOME/dev/helpful_scripts
-# Sets my custom keyboard layout
-setxkbmap -I$SCRIPTS/xkb/ dvorakprog -option caps:escape -geometry 'microsoft(natural)' -print | xkbcomp -I$SCRIPTS/xkb/ - $DISPLAY
